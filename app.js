ddfbb5d6eec30c64226e014ff2d659332fb0581a
Labinot Jaha <labinot@jaha.dev>
2022-06-12 13:17:47 +0200

    Finished Sketchpad v1.0

--- app.js
+++ app.js
@@ -4,25 +4,28 @@ let canvas = document.querySelector(".canvas");
 
 
 button.addEventListener("click", () =>{
-
+   
     var size = prompt("Test");
-    
+
     canvas.style.gridTemplateColumns = "repeat(" + size +", 50px)";
     canvas.style.width = size * 50 + "px";
     canvas.style.height = size * 50 + "px";
     for(i = 0; i < size * size; i++){
         
         let gridPanel = document.createElement("div");
-        gridPanel.textContent = i;
         gridPanel.style.width = 50 + "px";
         gridPanel.style.height = 50 + "px";
-        gridPanel.classList.add("div" + i);
-        gridPanel.classList.add("HoverClass");
+        gridPanel.classList.add("div");
         canvas.appendChild(gridPanel);
+        gridPanel.addEventListener("mouseover", () =>{
+            gridPanel.style.backgroundColor ="black";
+        })
+
+        gridPanel.addEventListener("mousedown", () =>{
+            gridPanel.style.backgroundColor ="white";
+        })
     }
     
+    
 })
-
-
-
 
